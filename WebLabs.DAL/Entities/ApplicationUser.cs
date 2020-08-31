4c9b29fbf770c9010a2b80fb3fca0c18e8dcbe96
Анастасия Ковалёнок <nastyakovalenok9@gmail.com>
2020-08-31 12:00:31 +0300

    Добавьте файлы проекта.

---
+++ WebLabs.DAL/Entities/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebLabs.DAL.Entities
+{
+    public class ApplicationUser: IdentityUser
+
+    {
+
+    }
+}
---
+++ WebLabs/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WebLabs.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<ListDemo> _listDemo;
+        //private List<ListDemo> SelectList;
+        public IActionResult Index()
+        {
+            
+            ViewData["Lst"] = new SelectList(_listDemo, "ListItemValue", "ListItemText");
+            ViewData["Text"] = "Лабораторная работа 2";
+            return View();
+        }
+
+        public HomeController()
+        {
+            _listDemo = new List<ListDemo>
+            {
+        new ListDemo{ ListItemValue=1, ListItemText="Item 1"},
+        new ListDemo{ ListItemValue=2, ListItemText="Item 2"},
+        new ListDemo{ ListItemValue=3, ListItemText="Item 3"}
+            };
+        }
+
+    }
+
+    public class ListDemo
+    {
+        public int ListItemValue { get; set; }
+        public string ListItemText { get; set; }
+    
+    }
+
+
+
+
+}
+
